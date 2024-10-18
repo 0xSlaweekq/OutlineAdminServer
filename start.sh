7b220f2590970981c7073160a37b03bd9a75deae
Slaweekq <celesik@gmail.com>
2024-10-19 01:56:32 +0700

    1

--- start.sh
+++ start.sh
@@ -19,6 +19,7 @@ echo Done! Create an A-type entry in your domain control panel, targeting $IP
 # alias defpass="echo 'YOUR_DOCKER_PASS"
 # alias doccon="docker login -u YOUR_DOCKER_USER_NAME --password-stdin"
 # defpass | doccon
+docker pull slaweekq/outline_admin:latest
 docker compose -p slaweekq stop || true
 docker compose -p slaweekq down -v && docker compose -f ./docker-compose.* rm -sfv
 # docker rmi $(docker images -q --no-trunc) || true
