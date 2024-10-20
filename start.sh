0d932b14558643400fc03c22e596f06b6344f968
Slaweekq <celesik@gmail.com>
2024-10-20 18:05:00 +0700

    1

--- start.sh
+++ start.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-sudo apt install -y certbot python3-certbot-nginx
-
-echo Enter the desired domain in the format of \"test.com\":
-read -a domain
+sudo apt update
+sudo apt install -y\
+  wget curl gpg net-tools nginx certbot python3-certbot-nginx apache2
 
 sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 443 -j ACCEPT
@@ -11,15 +10,6 @@ sudo iptables-save
 
 certbot certonly --standalone --agree-tos --register-unsafely-without-email -d ${domain[@]}
 certbot renew --dry-run
-
-# for elem in ${domain[@]}; do
-#    TRAEFIK_DOMAIN+=" \`$elem\`,"
-#    NGINX_DOMAIN+=" $elem"
-# done
-
-echo \`${domain[@]}\`
-sudo sed -i "s/Host([^)]*)/Host(\`${domain[@]}\`)/g" docker-compose.yml
-sudo sed -i "s/NGINX_HOST=[^)]*/NGINX_HOST=${domain[@]}/g" docker-compose.yml
 
 # start bash -i ./start.sh
 # alias defpass="echo 'YOUR_DOCKER_PASS"
@@ -31,8 +21,20 @@ docker compose -p slaweekq down -v && docker compose -f ./docker-compose.* rm -s
 # docker rmi $(docker images -q --no-trunc) || true
 docker compose -f ./docker-compose.* -p slaweekq up -d
 
-echo "Server started"
 IP=$(curl api.ipify.org)
 echo "################################################################"
-echo Done! Create an A-type entry in your domain control panel, targeting $IP
+echo Done, Server started! Create an A-type entry in your domain control panel, targeting $IP
 echo "################################################################"
+
+
+# echo Enter the desired domain in the format of \"test.com\":
+# read -a domain
+
+# for elem in ${domain[@]}; do
+#    TRAEFIK_DOMAIN+=" \`$elem\`,"
+#    NGINX_DOMAIN+=" $elem"
+# done
+
+# echo \`${domain[@]}\`
+# sudo sed -i "s/Host([^)]*)/Host(\`${domain[@]}\`)/g" docker-compose.yml
+# sudo sed -i "s/NGINX_HOST=[^)]*/NGINX_HOST=${domain[@]}/g" docker-compose.yml
