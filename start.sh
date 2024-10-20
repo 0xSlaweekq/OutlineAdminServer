84d2aab1a81e07662255922074e418fa0704f481
Slaweekq <celesik@gmail.com>
2024-10-20 21:19:20 +0700

    1

--- start.sh
+++ start.sh
@@ -4,8 +4,31 @@ sudo apt update
 sudo apt install -y\
   wget curl gpg net-tools nginx certbot python3-certbot-nginx apache2
 
+sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 8080 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 3128 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 2525 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 2053 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 3333 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 37280 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 58628 -j ACCEPT
+
+# sudo ufw allow 22/tcp
+# sudo ufw allow 443/tcp
+# sudo ufw allow 8080/tcp
+# # 3proxy
+# sudo ufw allow 3128/tcp
+# sudo ufw allow 2525/tcp
+# # 3xui
+# sudo ufw allow 2053/tcp
+# sudo ufw allow 3333/tcp
+# # outline
+# sudo ufw allow 37280/tcp
+# sudo ufw allow 58628/tcp
+# sudo ufw allow 58628/udp
+
 sudo iptables-save
 
 echo Enter the desired domain in the format of \"test.com\":
