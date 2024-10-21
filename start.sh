447983adc58ccbf2fb97272c9d02982cc3da7372
Slaweekq <celesik@gmail.com>
2024-10-21 13:21:48 +0700

    1

--- start.sh
+++ start.sh
@@ -46,17 +46,6 @@ certbot renew --dry-run
 # sudo sed -i "s/Host([^)]*)/Host(\`${domain[@]}\`)/g" docker-compose.yml
 # sudo sed -i "s/NGINX_HOST=[^)]*/NGINX_HOST=${domain[@]}/g" docker-compose.yml
 
-# start bash -i ./start.sh
-# alias defpass="echo 'YOUR_DOCKER_PASS"
-# alias doccon="docker login -u YOUR_DOCKER_USER_NAME --password-stdin"
-# defpass | doccon
-docker pull slaweekq/outline_admin:latest
-docker compose -f ./docker-compose.* stop || true
-docker compose -f ./docker-compose.* down -v && docker compose -f ./docker-compose.* rm -sfv
-# docker rmi $(docker images -q --no-trunc) || true
-docker compose -f ./docker-compose.* up -d
 
-IP=$(curl api.ipify.org)
-echo "################################################################"
-echo Done, Server started! Create an A-type entry in your domain control panel, targeting $IP
-echo "################################################################"
+chmod +x ./*.sh
+./init.sh
